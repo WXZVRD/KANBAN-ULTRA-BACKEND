fcdf897bd323a461d809b7e2a3301d42605d3283
wxzvd <forworknickworron@gmail.com>
2025-07-20 15:19:58 +0500

    Update: auth controller register Req

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { User } from '../user/entity/user.entity';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -9,7 +17,10 @@ export class AuthController {
 
   @Post('register')
   @HttpCode(HttpStatus.OK)
-  public async register(@Body() dto: RegisterDto): Promise<User | null> {
-    return this.authService.register(dto);
+  public async register(
+    @Req() req: Request,
+    @Body() dto: RegisterDto,
+  ): Promise<User | null> {
+    return this.authService.register(req, dto);
   }
 }
