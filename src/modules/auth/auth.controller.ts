decc3b51e93f31ebd9dce2615f3c8ead9290aaae
wxzvd <forworknickworron@gmail.com>
2025-07-20 14:27:58 +0500

    Add: auth controller register method implementation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { User } from '../user/entity/user.entity';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  @HttpCode(HttpStatus.OK)
+  public async register(@Body() dto: RegisterDto): Promise<User | null> {
+    return this.authService.register(dto);
+  }
 }
