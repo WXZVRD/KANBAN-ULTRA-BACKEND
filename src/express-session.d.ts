2b5a2aba27a8e82e59be37a61bbf2e3ca7cdd0ac
wxzvd <forworknickworron@gmail.com>
2025-07-20 15:19:19 +0500

    Extension: expres session extend

--- src/express-session.d.ts
+++ src/express-session.d.ts
@@ -1,7 +1,14 @@
 import 'express-session';
+import 'express';
 
 declare module 'express-session' {
-  interface SessionData {
-    userId?: string;
+  interface Session {
+    userId: number;
+  }
+}
+
+declare module 'express' {
+  interface Request {
+    session: Express.Session;
   }
 }
