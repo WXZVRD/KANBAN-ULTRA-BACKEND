f0ee0c4133c8bb40a2fc30b631792c7346f41fd3
wxzvd <forworknickworron@gmail.com>
2025-07-20 14:22:04 +0500

    Fix: user module providers and exports services

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
+import { UserService } from './user.service';
+import { UserRepository } from './user.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
+  providers: [UserService, UserRepository],
+  exports: [UserService],
 })
 export class UserModule {}
