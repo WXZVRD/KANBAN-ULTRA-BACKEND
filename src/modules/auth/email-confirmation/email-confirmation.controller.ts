50eb034f569f2ed6b143e095bdcab82b12782145
wxzvd <forworknickworron@gmail.com>
2025-07-27 19:10:09 +0500

    Add: email confirmation module

--- src/modules/auth/email-confirmation/email-confirmation.controller.ts
+++ src/modules/auth/email-confirmation/email-confirmation.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { EmailConfirmationService } from './email-confirmation.service';
+import { ConfirmationDto } from './dto/confirmation.dto';
+import { Request } from 'express';
 
-@Controller('email-confirmation')
+@Controller('auth/email-confirmation')
 export class EmailConfirmationController {
-  constructor(private readonly emailConfirmationService: EmailConfirmationService) {}
+  constructor(
+    private readonly emailConfirmationService: EmailConfirmationService,
+  ) {}
+
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  public async newVerification(
+    @Req() req: Request,
+    @Body() dto: ConfirmationDto,
+  ): Promise<void> {
+    return this.emailConfirmationService.newVerification(req, dto);
+  }
 }
--- src/modules/auth/email-confirmation/email-confirmation.module.ts
+++ src/modules/auth/email-confirmation/email-confirmation.module.ts
@@ -1,9 +1,29 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { EmailConfirmationService } from './email-confirmation.service';
 import { EmailConfirmationController } from './email-confirmation.controller';
+import { TokenRepository } from '../repositories/token.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Token } from '../../account/entity/token.entity';
+import { MailModule } from '../../mail/mail.module';
+import { AuthModule } from '../auth.module';
+import { UserModule } from '../../user/user.module';
+import { UserService } from '../../user/user.service';
+import { MailService } from '../../mail/mail.service';
 
 @Module({
+  imports: [
+    MailModule,
+    UserModule,
+    forwardRef(() => AuthModule),
+    TypeOrmModule.forFeature([Token]),
+  ],
   controllers: [EmailConfirmationController],
-  providers: [EmailConfirmationService],
+  providers: [
+    EmailConfirmationService,
+    UserService,
+    MailService,
+    TokenRepository,
+  ],
+  exports: [EmailConfirmationService, TokenRepository],
 })
 export class EmailConfirmationModule {}
