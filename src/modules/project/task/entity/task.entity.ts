7760b5b85117c8b997c98478f684d3b664c58d5f
wxzvd <forworknickworron@gmail.com>
2025-07-29 22:57:38 +0500

    Add: Task entity and repository

---
+++ src/modules/project/task/entity/task.entity.ts
@@ -0,0 +1,71 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { AccessType } from '../../types/access.enum';
+import { TaskPriority } from '../types/priority.enum';
+import { User } from '../../../user/entity/user.entity';
+import { ProjectColumn } from '../../column/entity/column.entity';
+import { Project } from '../../entity/project.entity';
+
+@Entity('task')
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({
+    enum: TaskPriority,
+    default: TaskPriority.MEDIUM,
+  })
+  priority: TaskPriority;
+
+  @ManyToOne(() => User, (user) => user.tasks, {
+    eager: true,
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'authorId' })
+  author: User;
+
+  @Column()
+  authorId: string;
+
+  @ManyToOne(() => User, { nullable: true, eager: true })
+  @JoinColumn({ name: 'assigneeId' })
+  assignee?: User;
+
+  @Column({
+    nullable: true,
+  })
+  assigneeId?: string;
+
+  @ManyToOne(() => ProjectColumn, (column) => column.tasks, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'columnId' })
+  column: ProjectColumn;
+
+  @Column()
+  columnId: string;
+
+  @ManyToOne(() => Project, (project) => project.tasks, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'projectId' })
+  project: Project;
+
+  @Column()
+  projectId: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
---
+++ src/modules/project/task/repository/task.repository.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeepPartial, Repository } from 'typeorm';
+import { Task } from '../entity/task.entity';
+
+@Injectable()
+export class TaskRepository {
+  public constructor(
+    @InjectRepository(Task)
+    private readonly repo: Repository<Task>,
+  ) {}
+
+  public async create(taskToCreate: DeepPartial<Task>): Promise<Task> {
+    return this.repo.create(taskToCreate);
+  }
+
+  public async save(taskToSave: Task): Promise<Task> {
+    return this.repo.save(taskToSave);
+  }
+
+  public async createAndSave(taskToSave: DeepPartial<Task>): Promise<Task> {
+    const createdTask: Task = this.repo.create(taskToSave);
+
+    return this.repo.save(createdTask);
+  }
+}
