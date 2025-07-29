7597faaf0033cd749450e5cf2d48a1e8c3ff6153
wxzvd <forworknickworron@gmail.com>
2025-07-29 20:57:50 +0500

    Add: project repository provide in module

--- src/modules/project/project.module.ts
+++ src/modules/project/project.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ProjectService } from './project.service';
+import { ProjectService } from './service/project.service';
 import { ProjectController } from './project.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entity/project.entity';
+import { ProjectRepository } from './repository/project.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Project])],
   controllers: [ProjectController],
-  providers: [ProjectService],
+  providers: [ProjectService, ProjectRepository],
 })
 export class ProjectModule {}
