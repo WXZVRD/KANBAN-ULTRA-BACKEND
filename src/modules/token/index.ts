840175aa29f7d16076cef03f3c877c3190bdd6ec
wxzvd <forworknickworron@gmail.com>
2025-08-02 00:35:46 +0500

    Add: create UserModule barrel file for imports

---
+++ src/modules/token/index.ts
@@ -0,0 +1,7 @@
+export * from './token.service';
+export * from './strategies/numeric-token.generator';
+export * from './strategies/uuid-token.generator';
+export * from './entity/token.entity';
+export * from './repository/token.repository';
+export * from './types/token.types';
+export * from './types/interfaces/token-generator.interface';
---
+++ src/modules/user/index.ts
@@ -0,0 +1,7 @@
+export * from './user.controller';
+export * from './services/user.service';
+export * from './repository/user.repository';
+export * from './dto/update-user.dto';
+export * from './types/roles.enum';
+export * from './types/authMethods.enum';
+export * from './entity/user.entity';
