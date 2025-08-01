420930ddf90064df7c32589933012d4daedcdd67
wxzvd <forworknickworron@gmail.com>
2025-08-02 00:39:05 +0500

    Refactor: make UserModule imports shortener

--- src/modules/project/project.module.ts
+++ src/modules/project/project.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ProjectService } from './service/project.service';
-import { ProjectController } from './project.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Project } from './entity/project.entity';
-import { ProjectRepository } from './repository/project.repository';
-import { ColumnModule } from './column/column.module';
-import { ProjectColumnService } from './column/column.service';
 import { UserModule } from '../user/user.module';
 import { TaskModule } from './task/task.module';
-import { MembershipModule } from './membership/membership.module';
-import { MembershipService } from './membership/services/membership.service';
+import {
+  Project,
+  ProjectController,
+  ProjectService,
+  ProjectRepository,
+} from './index';
+import { MembershipModule, MembershipService } from './membership';
+import { ColumnModule, ProjectColumnService } from './column';
 
 @Module({
   imports: [
