de7d5afd051811da125b22a8fc07f3cda8f49396
wxzvd <forworknickworron@gmail.com>
2025-08-03 16:24:35 +0500

    Add: Create barrel index file

---
+++ src/modules/auth/index.ts
@@ -0,0 +1,9 @@
+export { AuthMapSwagger } from './maps/auth-map.swagger';
+export { RegisterDto } from './dto/register.dto';
+export { LoginDto } from './dto/login.dto';
+export { AuthProviderGuard } from './guards/provider.guard';
+export { RolesGuard } from './guards/roles.guard';
+export { AuthGuard } from './guards/auth.guard';
+export { Roles, ROLES_KEY } from './decorators/roles.decorator';
+export { Authorized } from './decorators/authorized.decorator';
+export { Authorization } from './decorators/auth.decorator';
