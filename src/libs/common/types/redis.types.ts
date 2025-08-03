a117af84f27fa56e0791ca5be4cf7ea526e8c476
wxzvd <forworknickworron@gmail.com>
2025-08-03 17:20:25 +0500

    Update: redis keys for Task

--- src/libs/common/types/redis.types.ts
+++ src/libs/common/types/redis.types.ts
@@ -1,12 +1,24 @@
 import { Project } from '../../../modules/project';
+import { Task } from '../../../modules/project/task';
 
 export enum RedisKey {
   // Projects
   Project = 'project',
   ProjectAll = 'project:all',
+
+  // Tasks
+  Task = 'task',
+  TaskAll = 'task:all',
+  ProjectTasks = 'project:tasks',
 }
 
 export interface RedisValueMap {
+  // Projects
   [RedisKey.Project]: Project;
   [RedisKey.ProjectAll]: Project[];
+
+  // Tasks
+  [RedisKey.Task]: Task;
+  [RedisKey.TaskAll]: Task[];
+  [RedisKey.ProjectTasks]: Task[];
 }
